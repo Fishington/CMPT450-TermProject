082c9fd20c57f195193a8533a960e81a1f1be161
fishington <fishera34@mymacewan.ca>
2025-11-05 15:42:43 -0700

    Creation of Main Dashboard Home Page Component

--- cmpt450_angular_termproject/src/app/app.routes.ts
+++ cmpt450_angular_termproject/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
+import { MainDashboardPage } from './main-dashboard-page/main-dashboard-page';
+
 export const routes: Routes = [];
--- cmpt450_angular_termproject/src/app/app.ts
+++ cmpt450_angular_termproject/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainDashboardPage } from "./main-dashboard-page/main-dashboard-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MainDashboardPage],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('cmpt450_angular_termproject');

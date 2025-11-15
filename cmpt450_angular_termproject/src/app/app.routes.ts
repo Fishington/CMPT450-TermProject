a518afc61330f1f0a850fb23b8b52a78bd255f6b
Fishington <76424114+Fishington@users.noreply.github.com>
2025-11-14 20:26:51 -0700

    Saving for use on laptop

--- cmpt450_angular_termproject/src/app/app.routes.ts
+++ cmpt450_angular_termproject/src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { MainDashboardPage } from './main-dashboard-page/main-dashboard-page';
+import { GameDetails } from './game-details/game-details';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: MainDashboardPage },
+    { path: 'detail', component: GameDetails }    
+
+];
